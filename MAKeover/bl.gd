2b86b15d2d1c0813b8592e0785733923f7095ceb
izrulgaming0-oss <izrulgaming0@gmail.com>
2025-10-31 21:24:36 +0700

    fix multitower and wave

--- MAKeover/bl.gd
+++ MAKeover/bl.gd
@@ -1,24 +1,28 @@
 extends CharacterBody2D
 var goal
 var vell
+var tower_from 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if goal:
+func _physics_process(delta: float) -> void:
+	
+	if ! is_instance_valid(get_node(goal)):
+		get_tree().current_scene.get_node(tower_from).force_slain(goal)
+		queue_free()
+	elif goal:
 		var goal = get_node(goal).global_position
 		vell = position.direction_to(goal).normalized()
 
 	velocity = vell  * 500
-	print(velocity)
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
-		body.damage(10)
+		body.damage(80,tower_from)
 		queue_free()
-	pass # Replace with function body.
+	pass # Replace with function body
--- turret.gd
+++ turret.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 var roott = 0
 var enemy 
-
+var enemy_arr =[]
+var enemy_dict = {}
 @onready var path_bullet = preload("res://MAKeover/bl.tscn")
 var inter = 0.5
 
@@ -12,7 +13,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not enemy == null :
+	if ! enemy:
+		if enemy_arr.size() > 0 :
+			enemy = enemy_arr.front()
+	elif ! is_instance_valid(get_node(enemy)):
+		enemy = null
+	elif not enemy == null :
 		var pos_enmy= get_node(enemy).global_position
 		var anglee =position.angle_to_point(pos_enmy)
 		rotation = rotate_toward(rotation,anglee,delta * 12)
@@ -26,13 +32,16 @@ func _process(delta: float) -> void:
 
 
 func enemy_entered(body: Node2D) -> void:
-	if not enemy == null :
-		pass
-	elif body.is_in_group("enemy"):
-		enemy = get_path_to(body)
+	if body.is_in_group("enemy"):
+		var path_enemy = get_path_to(body)
+		enemy_arr.append(path_enemy)
+		enemy_dict.get_or_add(body.id,path_enemy)
 	pass # Replace with function body.
 func enemy_exit(body: Node2D) -> void:
-	if get_path_to(body) == enemy : enemy = null 
+	if get_path_to(body) == enemy : 
+		enemy_arr.erase(enemy)
+		enemy = null 
+		
 	
 	
 	pass # Replace with function body.
@@ -41,5 +50,21 @@ func enemy_exit(body: Node2D) -> void:
 func make_bullet():
 	var bl = path_bullet.instantiate()
 	bl.goal = enemy
+	bl.tower_from = get_tree().current_scene.get_path_to(self)
 	bl.position= position
 	get_parent().add_child(bl)
+
+func enemy_slain(path_id):
+	if path_id in enemy_dict:
+		enemy_arr.erase(enemy_dict[path_id])
+		if enemy == enemy_dict[path_id]:
+			enemy = null
+		enemy_dict.erase(path_id)
+
+func force_slain(obj):
+	if obj in enemy_arr:
+		if enemy_dict[enemy_arr.find(obj)]:
+			enemy_dict[enemy_arr.find(obj)]
+		enemy_arr.erase(obj)
+		enemy = null
+	
